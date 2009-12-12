01d6f2e79c5be823440f6482774a8efadaa39002
ingmar.runge <ingmar.runge@c7c0d16e-32bb-11de-8a2e-13aa1706fec1>
2009-12-12 20:18:19 +0000

    Moved WINVER and friends' definitions to a new header file (winver.h) and it included this from all places where windows.h etc. are used.

--- main/znc-msvc/main.h
+++ main/znc-msvc/main.h
@@ -73,18 +73,7 @@
 #define MODULECALL(macFUNC, macUSER, macCLIENT, macEXITER)
 #endif
 
-#ifdef _WIN32
-#define WINVER		0x0500
-#define _WIN32_WINNT   0x0500
-#define _WIN32_WINDOWS 0x0500
-#define _WIN32_IE      0x0600
-#define WIN32_LEAN_AND_MEAN
-#endif
-
-#ifdef WIN_MSVC
-#define VC_EXTRALEAN
-#endif
-
+#include "winver.h"
 #include "exports.h"
 
 #ifdef WIN_MSVC
