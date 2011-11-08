67d5b630b23da949e7983c4d357bf6adb5fc416c
ingmar.runge <ingmar.runge@c7c0d16e-32bb-11de-8a2e-13aa1706fec1>
2011-11-08 19:13:17 +0000

    Bump formal WINVER to XP, MSVC++2010 isn't Win2k compatible anyway, so Win2k has been broken since 0.098.

--- trunk/main/znc-msvc/win32/src/winver.h
+++ trunk/main/znc-msvc/win32/src/winver.h
@@ -1,9 +1,9 @@
 #pragma once
 
 #ifdef _WIN32
-#define WINVER		0x0500
-#define _WIN32_WINNT   0x0500
-#define _WIN32_WINDOWS 0x0500
+#define WINVER		0x0501
+#define _WIN32_WINNT   WINVER
+#define _WIN32_WINDOWS WINVER
 #define _WIN32_IE      0x0600
 #define WIN32_LEAN_AND_MEAN
 #endif
