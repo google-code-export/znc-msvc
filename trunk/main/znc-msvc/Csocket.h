c02200feaec742765e55427f1af34716ed694b65
ingmar.runge <ingmar.runge@c7c0d16e-32bb-11de-8a2e-13aa1706fec1>
2011-11-09 18:19:44 +0000

    enforce WSA* socket error codes instead of posix values => correct FormatMessage output from user-generated errors in Csocket.

--- trunk/main/znc-msvc/Csocket.h
+++ trunk/main/znc-msvc/Csocket.h
@@ -54,24 +54,82 @@
 #include <winsock2.h>
 #include <ws2tcpip.h>
 #include <sys/timeb.h>
-#ifndef ECONNREFUSED
-#define ECONNREFUSED WSAECONNREFUSED
-#endif
-#ifndef EINPROGRESS
-#define EINPROGRESS WSAEINPROGRESS
-#endif
-#ifndef ETIMEDOUT
-#define ETIMEDOUT WSAETIMEDOUT
-#endif
-#ifndef EADDRNOTAVAIL
-#define EADDRNOTAVAIL WSAEADDRNOTAVAIL
-#endif
-#ifndef ECONNABORTED
-#define ECONNABORTED WSAECONNABORTED
-#endif
-#ifndef ENETUNREACH
-#define ENETUNREACH WSAENETUNREACH
-#endif
+
+/* y hello there. need to this to have FormatMessage return the right error message codes where EADDRNOTAVAIL is used in Csocket code. */
+#undef EWOULDBLOCK
+#define EWOULDBLOCK             WSAEWOULDBLOCK
+#undef EINPROGRESS
+#define EINPROGRESS             WSAEINPROGRESS
+#undef EALREADY
+#define EALREADY                WSAEALREADY
+#undef ENOTSOCK
+#define ENOTSOCK                WSAENOTSOCK
+#undef EDESTADDRREQ
+#define EDESTADDRREQ            WSAEDESTADDRREQ
+#undef EMSGSIZE
+#define EMSGSIZE                WSAEMSGSIZE
+#undef EPROTOTYPE
+#define EPROTOTYPE              WSAEPROTOTYPE
+#undef ENOPROTOOPT
+#define ENOPROTOOPT             WSAENOPROTOOPT
+#undef EPROTONOSUPPORT
+#define EPROTONOSUPPORT         WSAEPROTONOSUPPORT
+#undef ESOCKTNOSUPPORT
+#define ESOCKTNOSUPPORT         WSAESOCKTNOSUPPORT
+#undef EOPNOTSUPP
+#define EOPNOTSUPP              WSAEOPNOTSUPP
+#undef EPFNOSUPPORT
+#define EPFNOSUPPORT            WSAEPFNOSUPPORT
+#undef EAFNOSUPPORT
+#define EAFNOSUPPORT            WSAEAFNOSUPPORT
+#undef EADDRINUSE
+#define EADDRINUSE              WSAEADDRINUSE
+#undef EADDRNOTAVAIL
+#define EADDRNOTAVAIL           WSAEADDRNOTAVAIL
+#undef ENETDOWN
+#define ENETDOWN                WSAENETDOWN
+#undef ENETUNREACH
+#define ENETUNREACH             WSAENETUNREACH
+#undef ENETRESET
+#define ENETRESET               WSAENETRESET
+#undef ECONNABORTED
+#define ECONNABORTED            WSAECONNABORTED
+#undef ECONNRESET
+#define ECONNRESET              WSAECONNRESET
+#undef ENOBUFS
+#define ENOBUFS                 WSAENOBUFS
+#undef EISCONN
+#define EISCONN                 WSAEISCONN
+#undef ENOTCONN
+#define ENOTCONN                WSAENOTCONN
+#undef ESHUTDOWN
+#define ESHUTDOWN               WSAESHUTDOWN
+#undef ETOOMANYREFS
+#define ETOOMANYREFS            WSAETOOMANYREFS
+#undef ETIMEDOUT
+#define ETIMEDOUT               WSAETIMEDOUT
+#undef ECONNREFUSED
+#define ECONNREFUSED            WSAECONNREFUSED
+#undef ELOOP
+#define ELOOP                   WSAELOOP
+#undef ENAMETOOLONG
+#define ENAMETOOLONG            WSAENAMETOOLONG
+#undef EHOSTDOWN
+#define EHOSTDOWN               WSAEHOSTDOWN
+#undef EHOSTUNREACH
+#define EHOSTUNREACH            WSAEHOSTUNREACH
+#undef ENOTEMPTY
+#define ENOTEMPTY               WSAENOTEMPTY
+#undef EPROCLIM
+#define EPROCLIM                WSAEPROCLIM
+#undef EUSERS
+#define EUSERS                  WSAEUSERS
+#undef EDQUOT
+#define EDQUOT                  WSAEDQUOT
+#undef ESTALE
+#define ESTALE                  WSAESTALE
+#undef EREMOTE
+#define EREMOTE                 WSAEREMOTE
 
 #endif /* _WIN32 */
 
