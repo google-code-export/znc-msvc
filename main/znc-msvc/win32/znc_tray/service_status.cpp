b1d51417f4a87f798895e94c6c822842f3d71856
ingmar.runge <ingmar.runge@c7c0d16e-32bb-11de-8a2e-13aa1706fec1>
2012-01-01 14:21:28 +0000

    implemented service status watcher for CServiceStatus (CServiceStatus::StartWatchingStatus)

--- main/znc-msvc/win32/znc_tray/service_status.cpp
+++ main/znc-msvc/win32/znc_tray/service_status.cpp
@@ -14,7 +14,8 @@
 
 CServiceStatus::CServiceStatus(const wchar_t* a_serviceName) :
 	m_serviceName(a_serviceName),
-	m_scm(NULL), m_hService(NULL)
+	m_scm(NULL), m_hService(NULL),
+	m_hwndWatch(NULL), m_hWatchThread(NULL), m_doWatch(false)
 {
 }
 
@@ -46,6 +47,23 @@ bool CServiceStatus::IsInstalled()
 }
 
 
+bool CServiceStatus::IsRunning()
+{
+	if(this->OpenService())
+	{
+		SERVICE_STATUS ss = {0};
+
+		if(::QueryServiceStatus(m_hService, &ss))
+		{
+			return (ss.dwCurrentState == SERVICE_RUNNING ||
+				ss.dwCurrentState == SERVICE_START_PENDING);
+		}
+	}
+
+	return false;
+}
+
+
 bool CServiceStatus::OpenService()
 {
 	if(m_hService)
@@ -53,7 +71,7 @@ bool CServiceStatus::OpenService()
 		return true;
 	}
 
-	m_hService = ::OpenService(m_scm, m_serviceName, STANDARD_RIGHTS_READ);
+	m_hService = ::OpenService(m_scm, m_serviceName, SERVICE_QUERY_STATUS);
 
 	return (m_hService != NULL);
 }
@@ -69,8 +87,161 @@ void CServiceStatus::CloseService()
 }
 
 
+bool CServiceStatus::StartWatchingStatus(HWND a_targetWindow)
+{
+	if(m_hWatchThread != NULL)
+	{
+		// already watching
+		return false;
+	}
+
+	m_hwndWatch = a_targetWindow;
+	m_doWatch = true;
+
+	m_hWatchThread = (HANDLE)_beginthreadex(NULL, 0, &WatchThreadProc, static_cast<void*>(this), 0, NULL);
+
+	return (!!m_hWatchThread);
+}
+
+
+unsigned __stdcall CServiceStatus::WatchThreadProc(void *ptr)
+{
+	// get instance:
+	CServiceStatus *instance = static_cast<CServiceStatus*>(ptr);
+
+	if(!instance)
+	{
+		return -1;
+	}
+
+	instance->WatchWait();
+
+	return 0;
+}
+
+
+void CServiceStatus::WatchWait()
+{
+	SC_HANDLE l_service = ::OpenService(m_scm, m_serviceName, SERVICE_QUERY_STATUS);
+
+	// check for OS support:
+	OSVERSIONINFOEXW l_osver = { sizeof(OSVERSIONINFOEXW), 0 };
+	l_osver.dwMajorVersion = 6;
+
+	DWORDLONG dwlVerCond = 0;
+	VER_SET_CONDITION(dwlVerCond, VER_MAJORVERSION, VER_GREATER_EQUAL);
+	VER_SET_CONDITION(dwlVerCond, VER_MINORVERSION, VER_GREATER_EQUAL);
+
+	if(::VerifyVersionInfoW(&l_osver, VER_MAJORVERSION | VER_MINORVERSION, dwlVerCond))
+	{
+		// the good version: NotifyServiceStatusChange
+		typedef DWORD (WINAPI *tfnssc)(SC_HANDLE hService, DWORD dwNotifyMask, PSERVICE_NOTIFY pNotifyBuffer);
+		tfnssc nssc = (tfnssc)::GetProcAddress(::GetModuleHandle(L"Advapi32.dll"), "NotifyServiceStatusChange");
+
+		if(!nssc) return; // shouldn't happen, we checked the OS version
+
+		SERVICE_NOTIFY serviceNotify = { 0 };
+		serviceNotify.dwVersion = SERVICE_NOTIFY_STATUS_CHANGE;
+		serviceNotify.pfnNotifyCallback = &ServiceNotifyCallback;
+		serviceNotify.pContext = static_cast<void*>(this);
+
+		while(m_doWatch)
+		{
+			if(nssc(l_service, SERVICE_NOTIFY_RUNNING | SERVICE_NOTIFY_START_PENDING |
+				SERVICE_NOTIFY_STOPPED | SERVICE_NOTIFY_STOP_PENDING, &serviceNotify) == ERROR_SUCCESS)
+			{
+				::SleepEx(INFINITE, TRUE); // Wait for the notification
+			}
+			else
+				break;
+		}
+	}
+	else
+	{
+		// for legacy OS (pre-Vista):
+
+		SERVICE_STATUS_PROCESS ssp = {0};
+		DWORD dwBytesNeeded = 0;
+		DWORD dwState = 0;
+
+		do
+		{
+			if(!::QueryServiceStatusEx(l_service, SC_STATUS_PROCESS_INFO, (LPBYTE)&ssp, sizeof(SERVICE_STATUS_PROCESS), &dwBytesNeeded))
+				break;
+
+			if(ssp.dwCurrentState != dwState)
+			{
+				this->OnWatchEvent(&ssp);
+
+				dwState = ssp.dwCurrentState;
+			}
+
+			::SleepEx(1000, TRUE); // probe every second
+		}
+		while(m_doWatch);
+	}
+
+	::CloseServiceHandle(l_service);
+}
+
+
+void CALLBACK CServiceStatus::ServiceNotifyCallback(void *ptr)
+{
+	SERVICE_NOTIFY* serviceNotify = static_cast<SERVICE_NOTIFY*>(ptr);
+	CServiceStatus *instance = static_cast<CServiceStatus*>(serviceNotify->pContext);
+
+	if(instance && serviceNotify->dwNotificationStatus == ERROR_SUCCESS)
+	{
+		instance->OnWatchEvent(&serviceNotify->ServiceStatus);
+	}
+}
+
+
+void CServiceStatus::OnWatchEvent(const LPSERVICE_STATUS_PROCESS pssp)
+{
+	// notify handler window:
+	switch(pssp->dwCurrentState)
+	{
+	case SERVICE_RUNNING:
+		::PostMessage(m_hwndWatch, WM_SERVICESTARTED, 0, 0);
+		break;
+	case SERVICE_START_PENDING:
+		::PostMessage(m_hwndWatch, WM_SERVICESTARTING, 0, 0);
+		break;
+	case SERVICE_STOP_PENDING:
+		::PostMessage(m_hwndWatch, WM_SERVICESTOPPING, 0, 0);
+		break;
+	case SERVICE_STOPPED:
+		::PostMessage(m_hwndWatch, WM_SERVICESTOPPED, 0, 0);
+		break;
+	}
+}
+
+
+static VOID CALLBACK APCDummyProc(ULONG_PTR) {}
+
+void CServiceStatus::StopWatchingStatus()
+{
+	if(m_doWatch && m_hWatchThread)
+	{
+		m_doWatch = false;
+
+		// post APC shit to wake up SleepEx:
+		::QueueUserAPC(&APCDummyProc, m_hWatchThread, NULL);
+
+		// wait till the thread has terminated:
+		::WaitForSingleObject(m_hWatchThread, INFINITE);
+		// ... then close the handle:
+		::CloseHandle(m_hWatchThread);
+		m_hWatchThread = NULL;
+	}
+}
+
+
 CServiceStatus::~CServiceStatus()
 {
+	this->StopWatchingStatus();
+
 	this->CloseService();
 
 	if(m_scm)
--- main/znc-msvc/win32/znc_tray/service_status.hpp
+++ main/znc-msvc/win32/znc_tray/service_status.hpp
@@ -8,6 +8,11 @@
 
 #pragma once
 
+#define WM_SERVICESTOPPED (WM_USER + 101)
+#define WM_SERVICESTARTED (WM_USER + 102)
+#define WM_SERVICESTARTING (WM_USER + 103)
+#define WM_SERVICESTOPPING (WM_USER + 104)
+
 class CServiceStatus
 {
 public:
@@ -16,11 +21,24 @@ public:
 
 	bool Init();
 	bool IsInstalled();
+	bool IsRunning();
+
+	bool StartWatchingStatus(HWND a_targetWindow);
+	void StopWatchingStatus();
 protected:
 	const wchar_t* m_serviceName;
 	SC_HANDLE m_scm;
 	SC_HANDLE m_hService;
 
+	HWND m_hwndWatch;
+	HANDLE m_hWatchThread;
+	bool m_doWatch;
+
 	bool OpenService();
 	void CloseService();
+
+	static unsigned __stdcall WatchThreadProc(void *ptr);
+	void WatchWait();
+	static void CALLBACK ServiceNotifyCallback(void *ptr);
+	void OnWatchEvent(const LPSERVICE_STATUS_PROCESS pss);
 };
